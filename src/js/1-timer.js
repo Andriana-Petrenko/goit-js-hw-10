8e47fe1276c8f42e079a0dfead26b5dcc81e0754
Andriana-Petrenko <zlata0707petrenko@gmail.com>
2024-01-25 11:04:32 +0200

    Update 1-timer.js

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -4,28 +4,79 @@ import iziToast from "izitoast";
 import 'izitoast/dist/css/iziToast.min.css';
 
 const inputPicker = document.querySelector('#datetime-picker');
-const button = document.querySelector('[data-start]');
-button.setAttribute('disabled','');
+const buttonStart = document.querySelector('[data-start]');
+const daysTimer = document.querySelector('[data-days]');
+const hoursTimer = document.querySelector('[data-hours]');
+const minutesTimer = document.querySelector('[data-minutes]');
+const secondsTimer = document.querySelector('[data-seconds]');
+buttonStart.setAttribute('disabled', '');
+let userSelectedDate = null;
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
     onClose(selectedDates) {
-      if (selectedDates[0]>Date.now()) {
-          button.removeAttribute('disabled');
-      } else {
+        userSelectedDate = selectedDates[0].getTime();
+        buttonStart.removeAttribute('disabled');
+      if (userSelectedDate<Date.now()) {
+          buttonStart.setAttribute('disabled', '');
           iziToast.error({
               iconUrl: '/img/close.png',
               position: 'topRight',
               message: 'Please choose a date in the future',
-});
-         button.setAttribute('disabled','');
-      }
-    console.log(selectedDates[0]);
-  },
+});          
+}   
+}
 };
 
 flatpickr(inputPicker, options);
-// let userSelectedDate = options.onClose;
-// console.log(userSelectedDate);
+
+
+buttonStart.addEventListener('click', onClickStart)
+
+function onClickStart() {
+    const intervalId=setInterval(() => {
+    let timeToLeft = userSelectedDate-Date.now();
+        
+      if (timeToLeft<=0) {
+          clearInterval(intervalId);
+          buttonStart.setAttribute('disabled','');
+          iziToast.info({
+              position: 'center',
+              message: 'It is your time!',
+          });
+          return;
+        };
+        
+    const { days, hours, minutes, seconds } = convertMs(timeToLeft);
+    daysTimer.textContent = `${addLeadingZero(days)}`;
+    hoursTimer.textContent = `${addLeadingZero(hours)}`;
+    minutesTimer.textContent = `${addLeadingZero(minutes)}`;
+    secondsTimer.textContent = `${addLeadingZero(seconds)}`;
+    }, 1000);
+}
+  
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+    return {days,hours,seconds,minutes};
+};
+
+function addLeadingZero(value){
+return value.toString().padStart(2, '0'); 
+}
+
