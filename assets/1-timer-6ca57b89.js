bac20b73d733bfe4b895b97c2e388f80bc6f6a20
Andriana-Petrenko <144230812+Andriana-Petrenko@users.noreply.github.com>
2024-01-25 09:04:59 +0000

    Deploying to gh-pages from @ Andriana-Petrenko/goit-js-hw-10@8e47fe1276c8f42e079a0dfead26b5dcc81e0754 🚀

--- assets/1-timer-6ca57b89.js
+++
@@ -1,2 +0,0 @@
-import{f as o,i}from"./vendor-651d7991.js";const r=document.querySelector("#datetime-picker"),e=document.querySelector("[data-start]");e.setAttribute("disabled","");const n={enableTime:!0,time_24hr:!0,defaultDate:new Date,minuteIncrement:1,onClose(t){t[0]>Date.now()?e.removeAttribute("disabled"):(i.error({iconUrl:"/img/close.png",position:"topRight",message:"Please choose a date in the future"}),e.setAttribute("disabled","")),console.log(t[0])}};o(r,n);
-//# sourceMappingURL=1-timer-6ca57b89.js.map
